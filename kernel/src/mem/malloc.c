393bbe073b2218a53b7d08f809a8830b2f9c28ea
Cold Fruit <yoda2031@gmail.com>
2023-10-18 17:24:58 +0100

    Finished implementing realloc

--- kernel/src/mem/malloc.c
+++ kernel/src/mem/malloc.c
@@ -5,6 +5,8 @@
 #include <mem/align.h>
 #include <string.h>
 
+void* big_realloc(void* ptr, size_t len);
+
 void *malloc(size_t len)
 {
     slab_t* slab = find_slab(8 + len);
@@ -21,7 +23,7 @@ void *malloc(size_t len)
 void* realloc(void* ptr, size_t len)
 {
     if(ptr == NULL) return malloc(len);
-    if(((uint64_t) ptr) & ((uint64_t) 0xfff) == 0)
+    if((((uint64_t) ptr) & ((uint64_t) 0xfff)) == 0)
     {
         return big_realloc(ptr, len);
     }
@@ -36,6 +38,39 @@ void* realloc(void* ptr, size_t len)
 
     return ptr;
 }
+
+void* big_realloc(void* ptr, size_t len)
+{
+    malloc_metadata_t* metadata = (malloc_metadata_t*)((uint64_t)ptr - PAGE_SIZE);
+    if(div_roundup(metadata->size, PAGE_SIZE) == div_roundup(len, PAGE_SIZE))
+    {
+        // the easiest resize, we just tell it to use the rest of the page it
+        // has already been allocated
+        metadata->size = len;
+        return ptr;
+    }
+    
+    void* new_ptr = malloc(len);
+    if (new_ptr == NULL) return NULL;
+
+
+    // copy data from the old region to the new region
+    if(metadata->size > len)
+    {
+        // in this mode, we will truncate the data (unavoidable, you're doing a big->small resize)
+        memcpy(new_ptr, ptr, len);
+    }
+    else
+    {
+        // in this mode, we will preserve all source data
+        memcpy(new_ptr, ptr, metadata->size);
+    }
+
+    free(ptr);
+
+    return new_ptr;
+}
+
 
 void* big_malloc(size_t len)
 {
